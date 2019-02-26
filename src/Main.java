aa1474a56c4a5db541abd467a3fb7b9e8690354e
Mark Roth <mzroth@gmail.com>
2019-02-26 16:47:22 -0500

    added placeShip

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,18 @@ public class Main {
         Cell[][] player1Cells = player1Board.getCells();
         player1Board.print();
         System.out.println(player1Board.checkShip());
-        player1Cells[1][2].setType(CellType.SHIP);
-        player1Board.print();
-        System.out.println(player1Board.checkShip());
 
         Player player1 = new Player("Mark");
         System.out.println(player1.getName());
+
+        Ship ship = new Ship(4, Direction.HORIZONTAL, player1Cells[1][2]);
+        ship.placeShip(player1Board);
+        player1Board.print();
+        System.out.println(player1Board.checkShip());
+
+        Ship ship2 = new Ship(3, Direction.VERTICAL, player1Cells[3][3]);
+        ship2.placeShip(player1Board);
+        player1Board.print();
 
     }
 }
--- src/Ship.java
+++ src/Ship.java
@@ -1,6 +1,33 @@
 public class Ship {
 
-    public Ship() {
+    private Integer length;
+    private Direction direction;
+    private Cell startCell;
 
+    public Ship (Integer length, Direction direction, Cell startCell) {
+        this.length = length;
+        this.direction = direction;
+        this.startCell = startCell;
     }
+
+    public void checkShip() {
+        
+    }
+
+    public void placeShip(Board board) {
+        switch (direction) {
+            case HORIZONTAL:
+                for (int i = 0; i < length; i++) {
+                    board.getCells()[startCell.getRow()][startCell.getCol() + i].setType(CellType.SHIP);
+                }
+                break;
+            default:
+                for (int i = 0; i < length; i++) {
+                    board.getCells()[startCell.getRow() + i][startCell.getCol()].setType(CellType.SHIP);
+                }
+                break;
+        }
+    }
+
+
 }
