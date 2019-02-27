e525f64c78ebd7480ceac98d3fce310e997f9723
Mark Roth <mzroth@gmail.com>
2019-02-27 16:06:10 -0500

    Got rid of the Ship class because it wasn't needed. I also added the canPlaceShip method

--- src/Board.java
+++ src/Board.java
@@ -25,7 +25,7 @@ public class Board {
     }
 
     //Checks to see if there is a SHIP still on the board
-    public Boolean checkShip() {
+    public Boolean checkForShip() {
         for (Cell[] cellRow : cells) {
             for (Cell cell : cellRow) {
                 if (cell.getType() == CellType.SHIP) {
@@ -52,6 +52,45 @@ public class Board {
         System.out.println(columnNames);
         for (int i = 0; i < 10; i++) {
             System.out.println(rowNums[i] + Arrays.toString(boardForPrint[i]));
+        }
+    }
+
+    //Checks to see if a ship can be placed. Eventually this will be private but for testing it is public
+    public boolean canPlaceShip(Cell startCell, Integer length, Direction direction) {
+        switch (direction) {
+            case HORIZONTAL:
+                if (startCell.getCol() + length > BOARD_SIZE) {
+                    return false;
+                } else {
+                    return true;
+                }
+            default:
+                if (startCell.getRow() + length > BOARD_SIZE) {
+                    return false;
+                } else {
+                    return true;
+                }
+        }
+    }
+
+    // Places a ship on the board
+    public void placeShip(Cell startCell, Integer length, Direction direction) {
+        boolean answer = canPlaceShip(startCell, length, direction);
+        if (!answer) {
+            System.out.println("You can't place a ship there!");
+        } else {
+            switch (direction) {
+                case HORIZONTAL:
+                    for (int i = 0; i < length; i++) {
+                        cells[startCell.getRow()][startCell.getCol() + i].setType(CellType.SHIP);
+                    }
+                    break;
+                default:
+                    for (int i = 0; i < length; i++) {
+                        cells[startCell.getRow() + i][startCell.getCol()].setType(CellType.SHIP);
+                    }
+                    break;
+            }
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         Board player1Board = new Board();
-        Cell[][] player1Cells = player1Board.getCells();
         player1Board.print();
-        System.out.println(player1Board.checkShip());
+        System.out.println("There is a ship: " + player1Board.checkForShip());
+
+        Cell cell1 = new Cell(1, 2, CellType.SHIP);
+        player1Board.placeShip(cell1, 4, Direction.HORIZONTAL);
+        player1Board.print();
+        Cell cell2 = new Cell(1, 7, CellType.SHIP);
+        player1Board.placeShip(cell2, 4, Direction.HORIZONTAL);
+        System.out.println("There is a ship: " + player1Board.checkForShip());
+
+        Cell cell3 = new Cell(3,3,CellType.SHIP);
+        player1Board.placeShip(cell3, 3, Direction.VERTICAL);
+        player1Board.print();
+        Cell cell4 = new Cell(8,3,CellType.SHIP);
+        player1Board.placeShip(cell4, 3, Direction.VERTICAL);
 
         Player player1 = new Player("Mark");
         System.out.println(player1.getName());
-
-        Ship ship = new Ship(4, Direction.HORIZONTAL, player1Cells[1][2]);
-        ship.placeShip(player1Board);
-        player1Board.print();
-        System.out.println(player1Board.checkShip());
-
-        Ship ship2 = new Ship(3, Direction.VERTICAL, player1Cells[3][3]);
-        ship2.placeShip(player1Board);
-        player1Board.print();
 
     }
 }
